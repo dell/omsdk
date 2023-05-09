cd607caff3b56bcda075a840e66f519fe432a836
John Moran <john.moran2@dell.com>
2023-05-09 10:19:49 -0400

    Bugfix for issue #33 - SNIMissingWarning

--- omsdk/http/sdkhttpep.py
+++ omsdk/http/sdkhttpep.py
@@ -25,7 +25,7 @@ import requests.adapters
 import requests.exceptions
 import requests.packages.urllib3
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
-from requests.packages.urllib3.exceptions import SNIMissingWarning
+#from requests.packages.urllib3.exceptions import SNIMissingWarning
 from requests.packages.urllib3.exceptions import InsecurePlatformWarning
 from omsdk.sdkcenum import EnumWrapper, TypeHelper
 import logging
